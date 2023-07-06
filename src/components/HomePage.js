611784fed161bbcc7b7f2440778511705581fc06
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-07-06 16:35:01 -0500

    Initial commit.

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PicOfMe from "./PicOfMe";
+
+export default function HomePage(){
+    return(
+        <div>
+            <PicOfMe />
+        </div>
+    )
+}
---
+++ src/components/PicOfMe.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "../Headshot_gray-blazer-black-shirt.jpg"
+
+export default function PicOfMe(){
+    return (
+        <img src={logo} alt="Headshot_gray-blazer-black-shirt" />
+    )
+}
